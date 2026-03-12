0b36e167c6bfbe124c01b38d495867c771d13d97
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2026-03-12 15:57:39 -0700

    Add primitive formatting

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
 
+type Definition struct {
+	Word     string `json:"word"`
+	Meanings []struct {
+		PartOfSpeech string `json:"partOfSpeech"`
+		Definitions  []struct {
+			Definition string `json:"definition"`
+			Example    string `json:"example"`
+		}
+	} `json:"meanings"`
+}
+
+func main() {
+	wordIn := "yes"
+	res, err := http.Get("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordIn)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("dict api error")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var defs []Definition
+
+	err = json.Unmarshal(body, &defs)
+	if err != nil {
+		panic(err)
+	}
+
+	def := defs[0]
+	meanings := def.Meanings
+	fmt.Print(def.Word + "\n\n")
+
+	for _, meaning := range meanings {
+		fmt.Printf("%s: \n\n", meaning.PartOfSpeech)
+		for _, defin := range meaning.Definitions {
+			fmt.Printf("%s\n\n", defin.Definition)
+			fmt.Printf("Example : %s\n\n", defin.Example)
+		}
+		fmt.Printf("___________________________\n\n")
+	}
 }
